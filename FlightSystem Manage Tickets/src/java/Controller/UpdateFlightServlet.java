f7074d9001da038d36bd3df4fee0811d7ce78b97
voleena <99241179+voleena@users.noreply.github.com>
2022-10-03 15:52:16 +0530

    added fight systems manage tickets back end
    added fight systems manage tickets back end to the main.

---
+++ FlightSystem Manage Tickets/src/java/Controller/UpdateFlightServlet.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Flight;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author dhanu
+ */
+public class UpdateFlightServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateFlightServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateFlightServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession httpSession = request.getSession();
+        
+        int from_oldCountryId = (Integer) httpSession.getAttribute("from_oldCountryId");
+        int to_oldCountryId = (Integer) httpSession.getAttribute("to_oldCountryId");
+        int flightId = Integer.parseInt(request.getParameter("id"));
+        String from = request.getParameter("from");
+        String to = request.getParameter("to");
+        String flight_type = request.getParameter("type");
+        String flight_date = request.getParameter("flight_date");
+        
+        Flight flight = new Flight();
+        FlightManage flightManage = new FlightManage();
+        
+        flight.setFrom_oldCountryId(from_oldCountryId);
+        flight.setTo_oldCountryId(to_oldCountryId);
+        flight.setFlight_id(flightId);
+        flight.setFrom(from);
+        flight.setTo(to);
+        flight.setFlight_type(flight_type);
+        flight.setFlight_date(flight_date);
+        
+        if (flightManage.updateFlight(flight) == 0) {
+            response.sendRedirect("staffDashboard_g1.jsp");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ FlightSystem/src/java/Model/Ticket.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author dhanu
+ */
+public class Ticket {
+    private int ticketId;
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(int ticketId) {
+        this.ticketId = ticketId;
+    }
+}
